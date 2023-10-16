0eb6dd85725a3cc0f8ed3e73eed92db48546bb2c
whamulti <whamulti@gmail.com>
2023-10-16 16:45:33 -0300

    instalacao_mysql_local_whaticket

---
+++ lib/_system.sh
@@ -0,0 +1,190 @@
+#!/bin/bash
+# 
+# system management
+
+#######################################
+# creates user
+# Arguments:
+#   None
+#######################################
+system_create_user() {
+  print_banner
+  printf "${WHITE} 💻 Agora, vamos criar o usuário para a instancia...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - root <<EOF
+  useradd -m -p $(openssl passwd -crypt ${mysql_root_password}) -s /bin/bash -G sudo deploy
+  usermod -aG sudo deploy
+EOF
+
+  sleep 2
+}
+
+#######################################
+# clones repostories using git
+# Arguments:
+#   None
+#######################################
+system_git_clone() {
+  print_banner
+  printf "${WHITE} 💻 Fazendo download do código Whamulti...${GRAY_LIGHT}"
+  printf "\n\n"
+
+
+  sleep 2
+
+  sudo su - deploy <<EOF
+  git clone ${link_git} /home/deploy/${instancia_add}/
+EOF
+
+  sleep 2
+}
+
+#######################################
+# installs outros
+# Arguments:
+#   None
+#######################################
+system_outros_install() {
+  print_banner
+  printf "${WHITE} 💻 Instalando outros...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - root <<EOF
+  apt install -y git
+  sudo timedatectl set-timezone America/Sao_Paulo
+  
+EOF
+
+  sleep 2
+}
+
+#######################################
+# installs node
+# Arguments:
+#   None
+#######################################
+system_node_install() {
+  print_banner
+  printf "${WHITE} 💻 Instalando nodejs...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - root <<EOF
+  curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+  apt install -y nodejs
+  
+EOF
+
+  sleep 2
+}
+
+#######################################
+# installs mysql
+# Arguments:
+#   None
+#######################################
+system_mysql_install() {
+  print_banner
+  printf "${WHITE} 💻 Instalando Mysql...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - root <<EOF
+  apt install -y mysql-server
+EOF
+
+  sleep 2
+}
+
+#######################################
+# Ask for file location containing
+# multiple URL for streaming.
+# Globals:
+#   WHITE
+#   GRAY_LIGHT
+#   BATCH_DIR
+#   PROJECT_ROOT
+# Arguments:
+#   None
+#######################################
+system_puppeteer_dependencies() {
+  print_banner
+  printf "${WHITE} 💻 Instalando puppeteer dependencies...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - root <<EOF
+  apt install -y libxshmfence-dev \
+                      libgbm-dev \
+                      wget \
+                      unzip \
+                      fontconfig \
+                      locales \
+                      gconf-service \
+                      libasound2 \
+                      libatk1.0-0 \
+                      libc6 \
+                      libcairo2 \
+                      libcups2 \
+                      libdbus-1-3 \
+                      libexpat1 \
+                      libfontconfig1 \
+                      libgcc1 \
+                      libgconf-2-4 \
+                      libgdk-pixbuf2.0-0 \
+                      libglib2.0-0 \
+                      libgtk-3-0 \
+                      libnspr4 \
+                      libpango-1.0-0 \
+                      libpangocairo-1.0-0 \
+                      libstdc++6 \
+                      libx11-6 \
+                      libx11-xcb1 \
+                      libxcb1 \
+                      libxcomposite1 \
+                      libxcursor1 \
+                      libxdamage1 \
+                      libxext6 \
+                      libxfixes3 \
+                      libxi6 \
+                      libxrandr2 \
+                      libxrender1 \
+                      libxss1 \
+                      libxtst6 \
+                      ca-certificates \
+                      fonts-liberation \
+                      libappindicator1 \
+                      libnss3 \
+                      xdg-utils
+EOF
+
+  sleep 2
+}
+
+#######################################
+# installs pm2
+# Arguments:
+#   None
+#######################################
+system_pm2_install() {
+  print_banner
+  printf "${WHITE} 💻 Instalando pm2...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - root <<EOF
+  npm install -g pm2
+
+EOF
+
+  sleep 2
+}
